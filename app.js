373bd16754a7617a6bf4ede48659a5e35bdc751f
Quentin Gliech <quentingliech@gmail.com>
2014-07-06 16:41:58 +0200

    Ajout d'un serveur Express qui délivre le contenu statique

--- app.js
+++ app.js
@@ -4,9 +4,18 @@
 
 
 var config = require("./config.json"),
-    io = require("socket.io").listen(config.port, { log: false }),
+    express = require('express'),
+    app = express(),
+    server = require("http").createServer(app),
+    compression = require("compression"),
+    io = require("socket.io").listen(server, { log: false }),
     TurtlesController = require("./lib/turtlesController");
 
 
 var controller = new TurtlesController(io, config);
 controller.init();
+
+app.use(compression())
+app.use("/", express.static(__dirname + "/dist"))
+
+server.listen(config.port);
